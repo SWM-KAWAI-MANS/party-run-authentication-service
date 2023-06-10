ae66c614e4259543b0e8925a6e827c65222d06c4
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-10 18:39:08 +0900

    feat: RefreshToken 예외사항 추가 및 테스트 #PAR-48

--- src/main/java/online/partyrun/partyrunauthenticationservice/domain/token/entity/RefreshToken.java
+++ src/main/java/online/partyrun/partyrunauthenticationservice/domain/token/entity/RefreshToken.java
@@ -1,18 +1,30 @@
 package online.partyrun.partyrunauthenticationservice.domain.token.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.util.StringUtils;
 
 @Getter
 @RedisHash(value = "refresh")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RefreshToken {
     @Id
-    private String value;
-    private String memberId;
+    String value;
+    String memberId;
 
     public RefreshToken(String value, String memberId) {
+        validateEmpty(value);
+        validateEmpty(memberId);
         this.value = value;
         this.memberId = memberId;
+    }
+
+    private void validateEmpty(String str) {
+        if (!StringUtils.hasText(str)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
---
+++ src/test/java/online/partyrun/partyrunauthenticationservice/domain/token/entity/RefreshTokenTest.java
@@ -0,0 +1,55 @@
+package online.partyrun.partyrunauthenticationservice.domain.token.entity;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("RefreshToken은")
+class RefreshTokenTest {
+    String value = "RefreshToken";
+    String memberId = "memberId";
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class value와_memberId를_받으면 {
+
+        @Test
+        @DisplayName("RefreshToken 엔티티를 생성한다.")
+        void createRefreshToken() {
+            assertThatCode(() -> new RefreshToken(value, memberId))
+                    .doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 잘못된_value를_받으면 {
+
+        @ParameterizedTest
+        @NullSource
+        @EmptySource
+        @DisplayName("예외를 반환한다.")
+        void throwException(String invalidValue) {
+            assertThatThrownBy(() -> new RefreshToken(invalidValue, memberId))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 잘못된_memberId를_받으면 {
+
+        @ParameterizedTest
+        @NullSource
+        @EmptySource
+        @DisplayName("예외를 반환한다.")
+        void throwException(String memberId) {
+            assertThatThrownBy(() -> new RefreshToken(value, memberId))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
