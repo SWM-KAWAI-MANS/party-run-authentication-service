f25f7435ef449846d01ccbbcf78f15aa30b9833a
박성우 <seongwoo0513@uos.ac.kr>
2023-06-11 21:53:35 +0900

    단위 테스트, 통합 테스트 추가 (#3)
    * chore : redis 의존성 추가 #PAR-48
    
    * feat: RefreshToken 예외사항 추가 및 테스트 #PAR-48
    
    * test: MemberService 테스트 추가 #PAR-48
    
    * feat : RefreshToken 예외 수정 #PAR-48
    
    * feat: FirebaseHandler 추가 #PAR-48-1
    
    * test : embedded redis 설정 #PAR-48-1
    
    * feat: 잘못된 idToken으로 인증하는 테스트 추가 #PAR-48-1
    
    * feat: 잘못된 idToken으로 인증하는 테스트 추가 #PAR-48-1
    
    * test: refreshToken으로 accessToken 재발급 테스트 추가 #PAR-48-1
    
    ---------
    
    Co-authored-by: Hyeonjun Park <phjppo0918@gmail.com>

--- src/main/java/online/partyrun/partyrunauthenticationservice/global/config/FirebaseConfig.java
+++ src/main/java/online/partyrun/partyrunauthenticationservice/global/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import online.partyrun.partyrunauthenticationservice.domain.auth.service.firebase.FirebaseHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -14,8 +15,8 @@ import java.io.IOException;
 public class FirebaseConfig {
 
     @Bean
-    public FirebaseAuth firebaseAuth() throws IOException {
-        return FirebaseAuth.getInstance(firebaseApp());
+    public FirebaseHandler firebaseHandler() throws IOException {
+        return new FirebaseHandler(FirebaseAuth.getInstance(firebaseApp()));
 
     }
     private FirebaseApp firebaseApp() throws IOException {
