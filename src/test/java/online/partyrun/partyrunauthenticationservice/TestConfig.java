3ea0cb80e09ae24641f973ca48770b04ba9ccf9b
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-08 17:14:33 +0900

    refactor: application.yml 환경 변수 변경 및 mock 빈 수정

--- src/test/java/online/partyrun/partyrunauthenticationservice/TestConfig.java
+++ src/test/java/online/partyrun/partyrunauthenticationservice/TestConfig.java
@@ -1,15 +1,21 @@
 package online.partyrun.partyrunauthenticationservice;
 
+
 import online.partyrun.partyrunauthenticationservice.domain.auth.service.firebase.FirebaseHandler;
 import online.partyrun.partyrunauthenticationservice.domain.member.event.MemberEventPublisher;
-
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 @TestConfiguration
 public class TestConfig {
 
-    @MockBean MemberEventPublisher memberEventPublisher;
+    @MockBean
+    SnsClient snsClient;
 
-    @MockBean FirebaseHandler firebaseHandler;
+    @MockBean
+    FirebaseHandler firebaseHandler;
+
+    @MockBean
+    MemberEventPublisher memberEventPublisher;
 }
