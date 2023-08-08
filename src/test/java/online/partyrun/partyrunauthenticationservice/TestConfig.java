d70cb9ab7a860131182f351b5ce4d1c187e075e1
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-08 16:42:41 +0900

    refactor: TestConfig에 mockBean 수정

--- src/test/java/online/partyrun/partyrunauthenticationservice/TestConfig.java
+++ src/test/java/online/partyrun/partyrunauthenticationservice/TestConfig.java
@@ -1,16 +1,17 @@
 package online.partyrun.partyrunauthenticationservice;
 
-import online.partyrun.partyrunauthenticationservice.domain.auth.service.firebase.FirebaseHandler;
 
+import online.partyrun.partyrunauthenticationservice.domain.auth.service.firebase.FirebaseHandler;
+import online.partyrun.partyrunauthenticationservice.domain.member.event.MemberEventPublisher;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-import software.amazon.awssdk.services.sns.SnsClient;
 
 @TestConfiguration
 public class TestConfig {
 
-    @MockBean SnsClient snsClient;
+    @MockBean
+    MemberEventPublisher memberEventPublisher;
 
-    @MockBean FirebaseHandler firebaseHandler;
+    @MockBean
+    FirebaseHandler firebaseHandler;
 }
